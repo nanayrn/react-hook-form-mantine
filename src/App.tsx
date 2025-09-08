7939ca5359c52c584812e72ce439f142bb237fc7
nanayrn <napatsawan.az@gmail.com>
2025-09-08 23:26:04 +0700

    chore: set up path aliases

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { MantineProvider } from "@mantine/core";
 import { BrowserRouter } from "react-router-dom";
-import AppRoutes from "./routes/Routes";
+
+import AppRoutes from "@routes/Routes";
 
 const App = () => {
   return (
