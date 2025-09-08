1ecac755a9795e06f48189504be08b61772a40d1
nanayrn <napatsawan.az@gmail.com>
2025-09-08 23:52:40 +0700

    feat: create home page

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import HealthCheck from "./HealthCheck";
+import Home from "./Home";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/health-check" element={<HealthCheck />} />
+      <Route path="/" element={<Home />} />
     </Routes>
   );
 };
