7736ea92b898999675ff1cb8f13278a133d4b315
nanayrn <napatsawan.az@gmail.com>
2025-10-02 23:43:39 +0700

    feat: testing modal notification on home page

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,5 +1,33 @@
+import { Link } from "react-router-dom";
+import { Button, Container } from "@mantine/core";
+
+import { useModalNotification } from "@contexts/ModalNotificationProvider";
+
 const Home = () => {
-  return <div>Home</div>;
+  const { showModalNotification } = useModalNotification();
+
+  return (
+    <Container>
+      <h1>Home Page</h1>
+      <Button
+        onClick={() =>
+          showModalNotification({
+            title: "Welcome!",
+            message: "This is a modal notification on Home page",
+            color: "green",
+          })
+        }
+      >
+        Show Modal Notification
+      </Button>
+
+      <div style={{ marginTop: 20 }}>
+        <Link to="/about">
+          <Button>Go to About Page</Button>
+        </Link>
+      </div>
+    </Container>
+  );
 };
 
 export default Home;
